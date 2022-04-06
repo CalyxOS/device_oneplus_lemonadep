CalyxOS/device_oneplus_lemonade

Initial calyx makefiles
Change-Id: Ia1c2050f8216ce75374d4b55c3bc94e9d3d97b4e

AndroidProducts.mk
@@ -15,9 +15,13 @@
 #
 
 PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/calyx_lemonade.mk \
     $(LOCAL_DIR)/lineage_lemonade.mk
 
 COMMON_LUNCH_CHOICES := \
+    calyx_lemonade-user \
+    calyx_lemonade-userdebug \
+    calyx_lemonade-eng \
     lineage_lemonade-user \
     lineage_lemonade-userdebug \
     lineage_lemonade-eng

CalyxOS/device_oneplus_lemonadep

AndroidProducts.mk
@@ -15,9 +15,13 @@
 #
 
 PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/calyx_lemonadep.mk \
     $(LOCAL_DIR)/lineage_lemonadep.mk
 
 COMMON_LUNCH_CHOICES := \
+    calyx_lemonadep-user \
+    calyx_lemonadep-userdebug \
+    calyx_lemonadep-eng \
     lineage_lemonadep-user \
     lineage_lemonadep-userdebug \
     lineage_lemonadep-eng
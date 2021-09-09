7157f2dc5e153cc3e0df8a0efcf5fd87f6f14610
Chirayu Desai <chirayudesai1@gmail.com>
2021-09-10 02:51:41 +0530

    Initial calyx makefiles
    Change-Id: Ia1c2050f8216ce75374d4b55c3bc94e9d3d97b4e

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,13 @@
 #
 
 PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/calyx_lemonadep.mk \
     $(LOCAL_DIR)/lineage_lemonadep.mk
 
 COMMON_LUNCH_CHOICES := \
+    calyx_lemonadep-user \
+    calyx_lemonadep-userdebug \
+    calyx_lemonadep-eng \
     lineage_lemonadep-user \
     lineage_lemonadep-userdebug \
     lineage_lemonadep-eng
---
+++ calyx_lemonadep.mk
@@ -0,0 +1,41 @@
+#
+# Copyright (C) 2021-2022 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit some common stuff.
+$(call inherit-product, vendor/calyx/config/common.mk)
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from lemonadep device
+$(call inherit-product, device/oneplus/lemonadep/device.mk)
+
+PRODUCT_NAME := calyx_lemonadep
+PRODUCT_DEVICE := lemonadep
+PRODUCT_MANUFACTURER := OnePlus
+PRODUCT_BRAND := OnePlus
+PRODUCT_MODEL := LE2123
+
+PRODUCT_SYSTEM_NAME := OnePlus9Pro_EEA
+PRODUCT_SYSTEM_DEVICE := OnePlus9Pro
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=OnePlus9Pro \
+    TARGET_PRODUCT=OnePlus9Pro_EEA \
+    PRIVATE_BUILD_DESC="OnePlus9Pro-user 11   release-keys"
+
+BUILD_FINGERPRINT := OnePlus/OnePlus9Pro_EEA/OnePlus9Pro:11/RKQ1.201105.002/2111112053:user/release-keys
